901f48f62dc6ef293eac84d99217f21c1686c1c5
Baixerastheo <theo.baixeras@gmail.com>
2026-01-27 14:55:56 +0100

    fix config next

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    // On force explicitement la racine du workspace pour éviter le warning
+    root: path.resolve(__dirname, ".."),
+  },
 };
 
 export default nextConfig;
