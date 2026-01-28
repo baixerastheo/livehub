d6d7ab346e5f7dd3c600c315b469b9ae6660a0d9
Lucas Gonzalez <lucas.gonz2702@gmail.com>
2026-01-28 16:50:52 +0100

    fix: module invitation non utilisé

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,13 +2,11 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module.js';
 import { ServerModule } from './server/server.module.js';
 import { CanalModule } from './canal/canal.module.js';
-import { InvitationService } from './invitation/invitation.service';
-import { InvitationController } from './invitation/invitation.controller';
-import { InvitationModule } from './invitation/invitation.module';
+import { InvitationModule } from './invitation/invitation.module.js';
 
 @Module({
   imports: [UserModule, ServerModule, CanalModule, InvitationModule],
-  providers: [InvitationService],
-  controllers: [InvitationController],
+  providers: [],
+  controllers: [],
 })
 export class AppModule {}
