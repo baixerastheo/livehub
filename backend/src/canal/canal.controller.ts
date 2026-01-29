1968fb63e34d357f14505ada44aacaeb67aa7972
Lucas Gonzalez <lucas.gonz2702@gmail.com>
2026-01-29 15:25:26 +0100

    fix: erreur de lint

--- backend/src/canal/canal.controller.ts
+++ backend/src/canal/canal.controller.ts
@@ -67,7 +67,7 @@ export class CanalController {
     @ApiNotFoundResponse({ 
         description: "Channel with this ID does not exist"
     })
-    async updateChannel(@Param('id', ParseIntPipe) id: number,@Body() data: UpdateCanal
+    async updateChannel(@Param('id', ParseIntPipe) id: number,@Body() data: UpdateCanal,
     ) {
         const result = await this.canalService.UpdateChannel(id, data);
         if (result.isErr()) {
@@ -89,5 +89,6 @@ export class CanalController {
             throw new NotFoundException(result.error);
         }
         return result.value;
+    
     }
 }
