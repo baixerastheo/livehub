436e4eddcf4ed2ce1f060607027a463052759222
Lucas Gonzalez <lucas.gonz2702@gmail.com>
2026-01-29 15:16:20 +0100

    fix: Ajout du CRUD message et canal

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,7 +6,7 @@ import { InvitationModule } from './invitation/invitation.module.js';
 import { MessageModule } from './message/message.module.js';
 
 @Module({
-  imports: [UserModule, ServerModule, CanalModule, InvitationModule, MessageModule],
+  imports: [UserModule, ServerModule, CanalModule, InvitationModule, MessageModule, MessageModule],
   providers: [],
   controllers: [],
 })
