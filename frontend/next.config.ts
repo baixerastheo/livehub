470cca4c78f6b722646df34fb62fef5294d07329
Baixerastheo <theo.baixeras@gmail.com>
2026-02-17 21:22:20 +0100

    Add dockerfile for frontend and compose

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
   turbopack: {
     // On force explicitement la racine du workspace pour éviter le warning
     root: path.resolve(__dirname, ".."),
